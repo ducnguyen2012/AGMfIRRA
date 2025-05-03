0d3588c1f82125c763298e392cb595f848b47bef
ducnguyen2012 <166124443+ducnguyen2012@users.noreply.github.com>
2025-05-03 18:57:55 +0700

    Create build.py

---
+++ model/build.py
@@ -0,0 +1,225 @@
+from model import objectives
+from .clip_model import Transformer, QuickGELU, LayerNorm, build_CLIP_from_openai_pretrained, convert_weights
+import numpy as np
+import torch
+import torch.nn as nn
+from collections import OrderedDict
+
+
+class IRRA(nn.Module):
+    def __init__(self, args, num_classes=11003):
+        super().__init__()
+        self.args = args
+        self.num_classes = num_classes
+        self._set_task()
+
+        self.base_model, base_cfg = build_CLIP_from_openai_pretrained(args.pretrain_choice, args.img_size, args.stride_size)
+        self.embed_dim = base_cfg['embed_dim']
+
+        self.logit_scale = torch.ones([]) * (1 / args.temperature) 
+
+        if 'id' in args.loss_names:
+            self.classifier = nn.Linear(self.embed_dim, self.num_classes)
+            nn.init.normal_(self.classifier.weight.data, std=0.001)
+            nn.init.constant_(self.classifier.bias.data, val=0.0)
+        if 'mim' in args.loss_names:
+            self.cross_attn = nn.MultiheadAttention(self.embed_dim,
+                                                    self.embed_dim // 64,
+                                                    batch_first=True)
+            self.cross_modal_transformer = Transformer(width=self.embed_dim,
+                                                       layers=args.cmt_depth,
+                                                       heads=self.embed_dim //
+                                                       64)
+            scale = self.cross_modal_transformer.width**-0.5
+            
+            self.ln_pre_t = LayerNorm(self.embed_dim)
+            self.ln_pre_i = LayerNorm(self.embed_dim)
+            self.ln_post = LayerNorm(self.embed_dim)
+
+            proj_std = scale * ((2 * self.cross_modal_transformer.layers)**-0.5)
+            attn_std = scale
+            fc_std = (2 * self.cross_modal_transformer.width)**-0.5
+            for block in self.cross_modal_transformer.resblocks:
+                nn.init.normal_(block.attn.in_proj_weight, std=attn_std)
+                nn.init.normal_(block.attn.out_proj.weight, std=proj_std)
+                nn.init.normal_(block.mlp.c_fc.weight, std=fc_std)
+                nn.init.normal_(block.mlp.c_proj.weight, std=proj_std)
+
+            # init cross attn
+            nn.init.normal_(self.cross_attn.in_proj_weight, std=attn_std)
+            nn.init.normal_(self.cross_attn.out_proj.weight, std=proj_std)
+            self.mim_head = nn.Sequential(
+                OrderedDict([
+                    ('dense', nn.Linear(self.embed_dim, self.embed_dim)),
+                    ('gelu', QuickGELU()),
+                    ('ln', LayerNorm(self.embed_dim)),
+                    ('fc', nn.Linear(self.embed_dim, args.patch_dim))  # patch_dim = 3 * patch_size^2
+                ])
+            )
+            nn.init.normal_(self.mim_head.dense.weight, std=fc_std)
+            nn.init.normal_(self.mim_head.fc.weight, std=proj_std)
+        if 'mlm' in args.loss_names:
+            self.cross_attn = nn.MultiheadAttention(self.embed_dim,
+                                                    self.embed_dim // 64,
+                                                    batch_first=True)
+            self.cross_modal_transformer = Transformer(width=self.embed_dim,
+                                                       layers=args.cmt_depth,
+                                                       heads=self.embed_dim //
+                                                       64)
+            scale = self.cross_modal_transformer.width**-0.5
+            
+            self.ln_pre_t = LayerNorm(self.embed_dim)
+            self.ln_pre_i = LayerNorm(self.embed_dim)
+            self.ln_post = LayerNorm(self.embed_dim)
+
+            proj_std = scale * ((2 * self.cross_modal_transformer.layers)**-0.5)
+            attn_std = scale
+            fc_std = (2 * self.cross_modal_transformer.width)**-0.5
+            for block in self.cross_modal_transformer.resblocks:
+                nn.init.normal_(block.attn.in_proj_weight, std=attn_std)
+                nn.init.normal_(block.attn.out_proj.weight, std=proj_std)
+                nn.init.normal_(block.mlp.c_fc.weight, std=fc_std)
+                nn.init.normal_(block.mlp.c_proj.weight, std=proj_std)
+
+            # init cross attn
+            nn.init.normal_(self.cross_attn.in_proj_weight, std=attn_std)
+            nn.init.normal_(self.cross_attn.out_proj.weight, std=proj_std)
+
+            self.mlm_head = nn.Sequential(
+                OrderedDict([('dense', nn.Linear(self.embed_dim, self.embed_dim)),
+                            ('gelu', QuickGELU()),
+                            ('ln', LayerNorm(self.embed_dim)),
+                            ('fc', nn.Linear(self.embed_dim, args.vocab_size))]))
+            # init mlm head
+            nn.init.normal_(self.mlm_head.dense.weight, std=fc_std)
+            nn.init.normal_(self.mlm_head.fc.weight, std=proj_std)
+
+    def _set_task(self):
+        loss_names = self.args.loss_names
+        self.current_task = [l.strip() for l in loss_names.split('+')]
+        print(f'Training Model with {self.current_task} tasks')
+    
+    
+    def cross_former(self, q, k, v):
+        x = self.cross_attn(
+                self.ln_pre_t(q),
+                self.ln_pre_i(k),
+                self.ln_pre_i(v),
+                need_weights=False)[0]
+        x = x.permute(1, 0, 2)  # NLD -> LND
+        x = self.cross_modal_transformer(x)
+        x = x.permute(1, 0, 2)  # LND -> NLD
+
+        x = self.ln_post(x)
+        return x
+
+    def encode_image(self, image):
+        x = self.base_model.encode_image(image)
+        return x[:, 0, :].float()
+        # return x.float() # for CLIP ResNet visual model
+
+    def encode_text(self, text):
+        x = self.base_model.encode_text(text)
+        return x[torch.arange(x.shape[0]), text.argmax(dim=-1)].float()
+    
+    def forward(self, batch):
+        def AGM(text_embeds, beta=0.95, tau=0.02, alpha1=0.1, alpha2=0.9,dim=-1):
+            '''
+            text_embedds: [Bx46x768]
+            This function return matrix of probability to mask the right position
+            '''
+            att_weights_origin = text_embeds
+            #1 implement softmax with tau:
+            att_weights = torch.nn.functional.softmax(att_weights_origin/tau, dim=dim) #! Bx46
+            att_weights = att_weights.unsqueeze(-1) #! Bx46x768
+            
+            #! multiply beta with last class attention weight
+            att_weights = beta*att_weights
+
+            ##! compute probability
+            probability_mask = alpha1 + alpha2*att_weights #! Bx46x768
+
+            text_embeds_m = probability_mask #! Bx46x768
+            return text_embeds_m
+        ret = dict()
+
+        images = batch['images']
+        caption_ids = batch['caption_ids']
+        image_feats, text_feats = self.base_model(images, caption_ids)
+        i_feats = image_feats[:, 0, :].float()
+        # i_feats = image_feats.float() # for CLIP ResNet visual model
+        t_feats = text_feats[torch.arange(text_feats.shape[0]), caption_ids.argmax(dim=-1)].float()
+
+        logit_scale = self.logit_scale
+        ret.update({'temperature': 1 / logit_scale})
+
+        if 'itc' in self.current_task:
+            ret.update({'itc_loss':objectives.compute_itc(i_feats, t_feats, logit_scale)})
+        
+        if 'sdm' in self.current_task:
+            ret.update({'sdm_loss':objectives.compute_sdm(i_feats, t_feats, batch['pids'], logit_scale)})
+        if 'cmt' in self.current_task:
+            ret.update({'cmt_loss':objectives.compute_cmt(i_feats, t_feats, batch['pids'], logit_scale)})
+        if 'cmpm' in self.current_task:
+            ret.update({'cmpm_loss':objectives.compute_cmpm(i_feats, t_feats, batch['pids'])})
+        
+        if 'id' in self.current_task:
+            image_logits = self.classifier(i_feats.half()).float()
+            text_logits = self.classifier(t_feats.half()).float()
+            ret.update({'id_loss':objectives.compute_id(image_logits, text_logits, batch['pids'])*self.args.id_loss_weight})
+
+            image_pred = torch.argmax(image_logits, dim=1)
+            text_pred = torch.argmax(text_logits, dim=1)
+
+            image_precision = (image_pred == batch['pids']).float().mean()
+            text_precision = (text_pred == batch['pids']).float().mean()
+            ret.update({'img_acc': image_precision})
+            ret.update({'txt_acc': text_precision})
+        
+        if 'mim' in self.current_task:
+            mim_ids = batch['image_ids']
+
+            mim_feats = self.base_model.encode_image(mim_ids)
+
+            x = self.cross_former(mim_feats, text_feats, text_feats)
+
+            x = self.mim_head(x)
+            scores = x.float().reshape(-1, self.args.vocab_size)
+            mim_labels = batch['mim_labels'].reshape(-1)
+            ret.update({'mim_loss': objectives.compute_mim(scores, mim_labels)*self.args.mim_loss_weight})
+
+            pred = scores.max(1)[1]
+            mim_label_idx = torch.nonzero(mim_labels)
+            acc = (pred[mim_label_idx] == mim_labels[mim_label_idx]).float().mean()
+            ret.update({'mim_acc': acc})
+
+            return ret
+        if 'mlm' in self.current_task:
+            mlm_ids = batch['mlm_ids']
+
+            mlm_feats = self.base_model.encode_text(mlm_ids) #! sau bước này mới chỉ ở encode text
+            # print(f"This is my mlm_feats: {mlm_feats}")
+
+            #! using AGM module
+            x = AGM(mlm_feats)
+            x = self.cross_former(mlm_feats, image_feats, image_feats)
+
+            x = self.mlm_head(x)  # [batch_size, text_len, num_colors]
+            #print(f"This is mlm torch after through MLM head: {x}")
+            scores = x.float().reshape(-1, self.args.vocab_size)
+            mlm_labels = batch['mlm_labels'].reshape(-1)
+            ret.update({'mlm_loss': objectives.compute_mlm(scores, mlm_labels)*self.args.mlm_loss_weight})
+
+            pred = scores.max(1)[1]
+            mlm_label_idx = torch.nonzero(mlm_labels)
+            acc = (pred[mlm_label_idx] == mlm_labels[mlm_label_idx]).float().mean()
+            ret.update({'mlm_acc': acc})
+
+        return ret
+
+
+def build_model(args, num_classes=11003):
+    model = IRRA(args, num_classes)
+    # covert model to fp16
+    convert_weights(model)
+    return model
